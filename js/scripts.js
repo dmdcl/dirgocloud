36a2e9c2ae3314b3c9eae717b5ff22fb8f3eb9e6
dmdcl <l21550321@chihuahua2.tecnm.mx>
2025-05-31 01:41:03 -0600

    Layout and CSS redesign

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,19 @@
-function openTab(tabName, element) {
-    // Oculta todos los elementos con clase tabcontent
-    var tabcontent = document.getElementsByClassName("tabcontent");
-    for (var i = 0; i < tabcontent.length; i++) {
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    
+    // Oculta todos los elementos con clase "tabcontent"
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
     
-    // Elimina la clase active de todos los botones
-    var tablinks = document.getElementsByClassName("tablink");
-    for (var i = 0; i < tablinks.length; i++) {
+    // Elimina la clase "active" de todos los botones
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
     
-    // Muestra la pestaña actual y añade la clase active al botón
+    // Muestra la pestaña actual y añade la clase "active" al botón
     document.getElementById(tabName).style.display = "block";
-    element.className += " active";
+    evt.currentTarget.className += " active";
 }
